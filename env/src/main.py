112f0b6d98fa448b2487b87dca128a525c118867
Isaac Cooke <cookeisaac2@gmail.com>
2023-11-13 19:52:33 +0000

    Added camera function

--- env/src/main.py
+++ env/src/main.py
@@ -1,1 +1,19 @@
-print('Hello World')
+from picamera import PiCamera
+from datetime import datetime, timedelta
+from orbit import satellite
+
+def capture(camera, image):
+    iss = satellite(25544)
+    iss_lat = iss.lat()
+    iss_long = iss.long()
+
+    south, exif_lat = convert(iss_lat)
+    west, exif_long = convert(iss_long)
+
+    camera.exif_tags['GPS.GPSLatitude'] = exif_lat
+    camera.exif_tags['GPS.GPSLatitudeRed'] = 's' if south else 'n'
+    camera.exif_tags['GPS.GPSLongitude'] = exif_long
+    camera.exif_tags['GPS.GPSLongitude'] = 'w' if west else 'e'
+
+    camera.capture(image)
+
